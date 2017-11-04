a256694aabd2f04446db8d6f063afd2968fcea79
Tue Dang <dangtritue@gmail.com>
2017-11-04 13:54:30 +0700

    translate bookstack to reveal format

--- resources/views/pages/presentation.blade.php
+++ resources/views/pages/presentation.blade.php
@@ -6,19 +6,31 @@
 
     <link rel="stylesheet" href="{{ baseUrl('/libs/reveal-js/css/reveal.css') }}"/>
     <link rel="stylesheet" href="{{ baseUrl('/libs/reveal-js/css/theme/beige.css') }}"/>
-    
-<!--    <script src="{{ baseUrl('/libs/jquery/jquery.min.js?version=2.1.4') }}"></script>-->
+
+    <script src="{{ baseUrl('/libs/jquery/jquery.min.js?version=2.1.4') }}"></script>
     <script src="{{ baseUrl('/libs/reveal-js/js/reveal.min.js?version=3.5.0') }}"></script>
-    
+
     @yield('head')
 </head>
 <body>
-
-{!! $page->html !!}
+    <div class='reveal'>
+        <div class='slides'>
+            {!! $page->html !!}
+        </div>
+    </div>
+    
 
 <script>
+    $slideRoot = $('.slides');
+    $('h2').each((index, h2) => {
+        $(h2)
+            .nextUntil('.slides>h2')
+            .andSelf()
+            .wrapAll("<section class='section'>");
+    });
+    $slideRoot.find(">*").not('section').remove();
+
     Reveal.initialize({});
-//    $('.reveal').prependTo("body");
 </script>
 </body>
 </html>
